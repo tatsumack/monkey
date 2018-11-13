c3fb16d8b2a01722e2f6f12a92dd7ff37ceab85d
tatsumack <tatsu.mack@gmail.com>
2018-11-13 23:22:45 +0900

    add repl

---
+++ repl/repl.go
@@ -0,0 +1,35 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		if line == "q" || line == "quit" {
+			fmt.Printf("quit repl\n")
+			break
+		}
+
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+
+}
