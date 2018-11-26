48b26db80a06569d43ba09232cccb11e88ed974d
tatsumack <tatsu.mack@gmail.com>
2018-11-27 00:04:14 +0900

    error object

--- object/object.go
+++ object/object.go
@@ -11,6 +11,7 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 )
 
 type Object interface {
@@ -58,3 +59,11 @@ func (rv *ReturnValue) Type() ObjectType {
 func (rv *ReturnValue) Inspect() string {
 	return rv.Value.Inspect()
 }
+
+// TODO Line Number
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
