2719465846140831fb0acb324fdb9adbc46d6168
tatsumack <tatsu.mack@gmail.com>
2018-12-16 15:10:50 +0900

    eval indexExpression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -383,3 +383,46 @@ func TestArrayLiterals(t *testing.T) {
 	testIntegerObject(t, arr.Elements[2], 6)
 
 }
+
+func TestArrayIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			"[1, 2, 3][0]",
+			1,
+		},
+		{
+			"[1, 2, 3][1]",
+			2,
+		},
+		{
+			"[1, 2, 3][2]",
+			3,
+		},
+		{
+			"[1, 2, 3][3]",
+			nil,
+		},
+		{
+			"[1, 2, 3][-1]",
+			nil,
+		},
+		{
+			"[1, 2, 3][1+1]",
+			3,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+
+}
