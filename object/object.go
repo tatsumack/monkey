3694dad4f8b5f1f4aa4b9370809a5469718bff66
tatsumack <tatsu.mack@gmail.com>
2018-12-23 11:53:57 +0900

    hash object

--- object/object.go
+++ object/object.go
@@ -20,6 +20,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	BUILTIN_OBJ      = "BUILTIN"
 	ARRAY_OBJ        = "ARRAY"
+	HASH_OBJ         = "HASH"
 )
 
 type Object interface {
@@ -153,6 +154,10 @@ type HashKey struct {
 	Value uint64
 }
 
+type Hashable interface {
+	HashKey() HashKey
+}
+
 func (b *Boolean) HashKey() HashKey {
 	var value uint64
 	if b.Value {
@@ -172,5 +177,30 @@ func (s *String) HashKey() HashKey {
 	h := fnv.New64a()
 	h.Write([]byte(s.Value))
 	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+type HashPair struct {
+	Key   Object
+	Value Object
+}
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ","))
+	out.WriteString("}")
+
+	return out.String()
 }
 
