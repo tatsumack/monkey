7abf5c6df92ce5dde68cd7ec63a3ee4cdc2c3627
tatsumack <tatsu.mack@gmail.com>
2018-12-23 12:58:42 +0900

    hash index expression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -216,6 +216,11 @@ if (10 > 1) {
 			`"Hello" - "World"`,
 			"unknown operator: STRING - STRING",
 		},
+		{
+			`{"name": "Monkey"}[fn(x) { x }];`,
+			"unusable as hash key: FUNCTION",
+
+		},
 	}
 
 	for _, tt := range tests {
@@ -450,7 +455,7 @@ func TestHashLiterals(t *testing.T) {
 		(&object.String{Value: "three"}).HashKey(): 3,
 		(&object.Integer{Value: 4}).HashKey():      4,
 		TRUE.HashKey():                             5,
-		FALSE.HashKey():                             6,
+		FALSE.HashKey():                            6,
 	}
 
 	if len(result.Pairs) != len(expected) {
@@ -464,6 +469,53 @@ func TestHashLiterals(t *testing.T) {
 		}
 
 		testIntegerObject(t, pair.Value, expectedValue)
+	}
+
+}
+
+func TestHashIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			`{"foo":5}["foo"]`,
+			5,
+		},
+		{
+			`{"foo":5}["bar"]`,
+			nil,
+		},
+		{
+			`let key = "foo"; {"foo":5}[key]`,
+			5,
+		},
+		{
+			`{}["foo"]`,
+			nil,
+		},
+		{
+			`{5:5}[5]`,
+			5,
+		},
+		{
+			`{true:5}[true]`,
+			5,
+		},
+		{
+			`{false:5}[false]`,
+			5,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
 	}
 
 }
